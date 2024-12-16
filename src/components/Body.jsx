ff690e07da3736d1dcc3d0cd46ffd472687adf19
unknown <2000sharma.ss@gmail.com>
2024-12-16 14:06:23 +0530

    adding hooks and improving css

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,42 @@
 import RestaurantCard from "./RestaurantCard";
 import restList from "../../utils/mockData";
+import { useState } from "react";
+
 const Body = () => {
+  const [resDetail, setResDetail] = useState(restList);
   return (
     <div className="body-comp">
       <div>
-        <button className="top-rated-btn" onClick={() => {}}>
+        <button
+          className="top-rated-btn"
+          onClick={() => {
+            {
+              let topResList = resDetail.filter((ele, val) => {
+                return ele.info.avgRatingString > "4.5";
+              });
+              console.log(topResList);
+              setResDetail(topResList);
+            }
+          }}
+        >
           Filter top rated
+        </button>
+
+        <button
+          className="pure-veg"
+          onClick={() => {
+            let pureVeg = resDetail.filter((res) => {
+              return res.info?.veg == true;
+            });
+            return setResDetail(pureVeg);
+          }}
+        >
+          Pure Veg
         </button>
       </div>
       <div className="res-container">
-        {restList.map((ele, val) => {
-          return <RestaurantCard key={val} resData={restList[val].info} />;
+        {resDetail.map((ele, val) => {
+          return <RestaurantCard key={val} resData={ele.info} />;
         })}
       </div>
     </div>
