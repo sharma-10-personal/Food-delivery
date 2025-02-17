426147de74d26c7b1d2d8715fbd5999cecc85979
unknown <2000sharma.ss@gmail.com>
2025-02-18 01:50:55 +0530

    adding lazy loading for about page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
-import About from "./components/About";
+//import About from "./components/About";
 import Contact from "./components/Contact";
 import ErrorPage from "./components/ErrorPage";
 import RestaurantMenu from "./components/RestaurantMenu";
 //import  from "./components/Groceries";
 
 const Groceries = lazy(() => import("./components/Groceries"));
+const About = lazy(() => import("./components/About"));
 
 const Applayout = () => {
   return (
@@ -27,7 +28,14 @@ const appRouter = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <Body /> },
-      { path: "/about", element: <About /> },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>loading about us</h1>}>
+            <About />{" "}
+          </Suspense>
+        ),
+      },
       { path: "/contact", element: <Contact /> },
       {
         path: "/groceries",
